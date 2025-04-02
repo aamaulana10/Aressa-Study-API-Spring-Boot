c060ec2e30c053d303e7a8b665354a812ee28986
Aa Maulana <aamaulanabola10@gmail.com>
2025-04-02 21:33:45 +0700

    setup project

---
+++ src/main/java/com/aressa/aressarestapi/AressarestapiApplication.java
@@ -0,0 +1,13 @@
+package com.aressa.aressarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AressarestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AressarestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aressa/aressarestapi/AressarestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aressa.aressarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AressarestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
